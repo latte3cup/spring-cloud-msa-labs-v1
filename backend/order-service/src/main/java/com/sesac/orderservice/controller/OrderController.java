d49ac88a57c480384c5fbed3d17dfbc02835a51f
mz\user <jslovejs182@gmail.com>
2025-08-11 11:52:08 +0900

    fix: @RequestBody 추가

--- backend/order-service/src/main/java/com/sesac/orderservice/controller/OrderController.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/controller/OrderController.java
@@ -28,7 +28,7 @@ public class OrderController {
     }
 
     @PostMapping
-    public ResponseEntity<Order> createOrder(OrderRequestDto request) {
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequestDto request) {
         try {
             Order order = orderService.createOrder(request);
             return ResponseEntity.status(HttpStatus.CREATED).body(order);
